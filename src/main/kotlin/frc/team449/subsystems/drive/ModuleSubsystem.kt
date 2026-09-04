2295a2a8244a79b9932e25f6134fc1b649cd5afa
s-zhdanova <sophia.m.zhdanova@gmail.com>
2026-09-03 21:20:29 -0400

    fix

--- src/main/kotlin/frc/team449/subsystems/drive/ModuleSubsystem.kt
+++ src/main/kotlin/frc/team449/subsystems/drive/ModuleSubsystem.kt
@@ -2,4 +2,10 @@ package frc.team449.subsystems.drive
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase
 
-class ModuleSubsystem : SubsystemBase()
+class ModuleSubsystem(private val io: ModuleIO) : SubsystemBase() {
+    private val inputs: ModuleIOInputsAutoLogged = ModuleIoInputsAutoLogged()
+
+    override fun periodic() {
+        io.updateInputs(inputs)
+    }
+}
