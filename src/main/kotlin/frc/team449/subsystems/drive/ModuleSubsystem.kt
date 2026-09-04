9a591c8098ccd4b50e733a7238862a07f6a2577d
s-zhdanova <sophia.m.zhdanova@gmail.com>
2026-09-03 21:19:39 -0400

    commands

--- src/main/kotlin/frc/team449/subsystems/drive/ModuleSubsystem.kt
+++ src/main/kotlin/frc/team449/subsystems/drive/ModuleSubsystem.kt
@@ -1,5 +1,18 @@
 package frc.team449.subsystems.drive
 
+import edu.wpi.first.wpilibj2.command.Command
 import edu.wpi.first.wpilibj2.command.SubsystemBase
 
-class ModuleSubsystem : SubsystemBase()
+class ModuleSubsystem(private val io: ModuleIO) : SubsystemBase() {
+    private val inputs: ModuleIOInputsAutoLogged = ModuleIoInputsAutoLogged()
+
+    override fun periodic() {
+        io.updateInputs(inputs)
+    }
+
+    fun setTurnVoltage(voltage: Double): Command =
+        runOnce { io.setVoltageTurn(voltage) }
+
+    fun stopTurn(): Command =
+        runOnce { io.setVoltageTurn(0.0) }
+}
