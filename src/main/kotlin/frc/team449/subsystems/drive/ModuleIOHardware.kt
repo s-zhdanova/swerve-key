5365baa610958f74ff200934687c549f958c1dee
s-zhdanova <sophia.m.zhdanova@gmail.com>
2026-09-03 21:12:19 -0400

    akit fix

--- src/main/kotlin/frc/team449/subsystems/drive/ModuleIOHardware.kt
+++ src/main/kotlin/frc/team449/subsystems/drive/ModuleIOHardware.kt
@@ -1,18 +1,4 @@
 package frc.team449.subsystems.drive
 
-import com.ctre.phoenix6.hardware.TalonFX
-import edu.wpi.first.units.Units
-
 open class ModuleIOHardware : ModuleIO {
-    val turnMotor = TalonFX(1)
-
-    override fun updateInputs(inputs: ModuleIO.ModuleIOInputs) {
-        inputs.turningVoltage = turnMotor.supplyVoltage.value.`in`(Units.Volts)
-        inputs.turningVelocity = turnMotor.velocity.value.`in`(Units.RadiansPerSecond)
-        inputs.turningAngle = turnMotor.position.value.`in`(Units.Radians)
-    }
-
-    override fun setVoltageTurn(voltage: Double) {
-        turnMotor.setVoltage(voltage)
-    }
 }
