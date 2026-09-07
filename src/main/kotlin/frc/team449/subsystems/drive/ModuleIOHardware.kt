c912b378adb06068a8cacaca4c98d4e7bd352bff
s-zhdanova <sophia.m.zhdanova@gmail.com>
2026-09-07 11:14:32 -0400

    new

--- src/main/kotlin/frc/team449/subsystems/drive/ModuleIOHardware.kt
+++ src/main/kotlin/frc/team449/subsystems/drive/ModuleIOHardware.kt
@@ -1,19 +1,3 @@
 package frc.team449.subsystems.drive
 
-import com.ctre.phoenix6.hardware.TalonFX
-import edu.wpi.first.math.geometry.Rotation2d
-import edu.wpi.first.units.Units
-
-open class ModuleIOHardware(turnID: Int, driveID: Int) : ModuleIO {
-    val turnMotor = TalonFX(turnID)
-
-    override fun updateInputs(inputs: ModuleIO.ModuleIOInputs) {
-        inputs.turningVoltage = turnMotor.supplyVoltage.value.`in`(Units.Volts)
-        inputs.turningVelocity = turnMotor.velocity.value.`in`(Units.RadiansPerSecond)
-        inputs.turningAngle = Rotation2d(turnMotor.position.value.`in`(Units.Radians))
-    }
-
-    override fun setVoltageTurn(voltage: Double) {
-        turnMotor.setVoltage(voltage)
-    }
-}
+open class ModuleIOHardware(turnID: Int, driveID: Int) : ModuleIO
