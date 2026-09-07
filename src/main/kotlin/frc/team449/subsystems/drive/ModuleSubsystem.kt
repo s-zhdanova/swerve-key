f3a66e5eff40883a353687cbd1f9af37e8a39a54
s-zhdanova <sophia.m.zhdanova@gmail.com>
2026-09-07 11:05:48 -0400

    new

--- src/main/kotlin/frc/team449/subsystems/drive/ModuleSubsystem.kt
+++ src/main/kotlin/frc/team449/subsystems/drive/ModuleSubsystem.kt
@@ -1,23 +1,14 @@
 package frc.team449.subsystems.drive
 
-import edu.wpi.first.math.controller.PIDController
 import edu.wpi.first.wpilibj2.command.Command
 import edu.wpi.first.wpilibj2.command.SubsystemBase
 
 class ModuleSubsystem(private val io: ModuleIO) : SubsystemBase() {
     private val inputs: ModuleIOInputsAutoLogged = ModuleIOInputsAutoLogged()
 
-    private val turnController = PIDController(0.5, 0.0, 0.05)
-
     override fun periodic() {
         io.updateInputs(inputs)
-        io.setVoltageTurn(turnController.calculate(inputs.turningAngle.radians))
     }
-
-    fun setAngle(rads: Double): Command =
-        runOnce {
-            turnController.setpoint = rads
-        }
 
     fun setTurnVoltage(voltage: Double): Command =
         runOnce {
