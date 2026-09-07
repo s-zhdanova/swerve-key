0ff98eea63c939adb31ef28faac49c846c57204a
s-zhdanova <sophia.m.zhdanova@gmail.com>
2026-09-07 11:07:28 -0400

    new

--- src/main/kotlin/frc/team449/subsystems/drive/ModuleIOSim.kt
+++ src/main/kotlin/frc/team449/subsystems/drive/ModuleIOSim.kt
@@ -1,29 +1,3 @@
 package frc.team449.subsystems.drive
 
-import edu.wpi.first.math.geometry.Rotation2d
-import edu.wpi.first.math.system.plant.DCMotor
-import edu.wpi.first.math.system.plant.LinearSystemId
-import edu.wpi.first.wpilibj.simulation.DCMotorSim
-
-open class ModuleIOSim : ModuleIO {
-    val turnMotorSim: DCMotorSim =
-        DCMotorSim(
-            LinearSystemId.createDCMotorSystem(
-                DCMotor.getKrakenX60(1),
-                0.005, // placeholder
-                3.0, // placeholder
-            ),
-            DCMotor.getKrakenX60(1)
-        )
-
-    override fun updateInputs(inputs: ModuleIO.ModuleIOInputs) {
-        inputs.turningVoltage = turnMotorSim.inputVoltage
-        inputs.turningVelocity = turnMotorSim.angularVelocityRadPerSec
-        inputs.turningAngle = Rotation2d(turnMotorSim.angularPositionRad)
-        turnMotorSim.update(0.02)
-    }
-
-    override fun setVoltageTurn(voltage: Double) {
-        turnMotorSim.inputVoltage = voltage
-    }
-}
+open class ModuleIOSim : ModuleIO
